00c4124d26a42092dd7603c18861f4bd01d1f770
karysto <thanasi.karachotzitis@mail.utoronto.ca>
2015-01-20 01:25:30 -0500

    Retrieve Twilio request body.

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const
-    Hapi       = require('hapi')
-  , accountSid = 'ACC_SID'
-  , authToken  = 'AUTH_TOKEN'
-  , client     = require('twilio')(accountSid, authToken);
+    Hapi        = require('hapi')
+  , querystring = require('querystring')
+  , accountSid  = 'ACC_SID'
+  , authToken   = 'AUTH_TOKEN'
+  , client      = require('twilio')(accountSid, authToken);
 
 // Configure the Hapi server
 var server = new Hapi.Server();
@@ -20,16 +21,19 @@ server.route([
     method: 'GET',
     path: '/twilioCall',
     handler: function(request, reply) {
+      var twilioRequest = request.query;
 
+      /* Text the client */
       client.messages.create({
-        to:'USER_NUMBER',
-        from:'TWILIO_NUMBER',
-        body: 'some static message'
+        to: twilioRequest.From,
+        from: twilioRequest.To,
+        body: 'Texted: ' + twilioRequest.Body
       }, function(error, message) {
         if (error) {
           console.log(error.message);
         }
       });
+
       reply('twilioCall executed');
     }
   }
