e33528254ef9ad7039bc291305017903ff567ac7
karysto <thanasi.karachotzitis@mail.utoronto.ca>
2015-01-22 02:06:58 -0500

    Remove old parser code. Import parser module. Clean up twilio entry route.

--- helper.js
+++ helper.js
@@ -1,16 +1,21 @@
 const
   data = require('./data.json');
 
-/* Returns the parsed request body for the determined phrase */
+/* Returns the parsed request body and data for the determined phrase */
 exports.parseRequestBody = function(request, callback) {
-  var twilioBody  = request.query.Body.split(' ')
-    , keyword     = twilioBody[0];
+    /* Clean and parse the request body */
+    var twilioBody  = request.query.Body.split(' ');
+    twilioBody      = twilioBody.map(function(element) {
+      return element.toLowerCase().trim();
+    });
+    var keyword     = twilioBody[0];
 
-    /* Parse the body according to the data being requested. */
+    /* Retrieve the requested data based on the parsed body. */
     switch (keyword) {
       case 'learn':
-        var content    = twilioBody.slice(1, twilioBody.length - 1)
-          , method     = twilioBody[twilioBody.length - 1]
+        /* Get the requested term, which is after keyword and before last word */
+        var content    = twilioBody.slice(1, twilioBody.length - 2).join(' ')
+          , method     = twilioBody[twilioBody.length - 2]
           , resultData = data[keyword][content][method].join('\n');
         break;
 
@@ -32,6 +37,10 @@ exports.parseRequestBody = function(request, callback) {
 
       case 'help':
         // give tutorial of possible commands
+        break;
+
+      default:
+        resultData = "There was a problem :( text help for instructions on using FetchrBot.";
         break;
     }
     callback(null, resultData);
--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const
   , Hapi        = require('hapi')
   , querystring = require('querystring')
   , data        = require('./data.json')
+  , helper      = require('./helper.js')
   , client      = require('twilio')(accountSid, authToken);
 
 /* Configure the Hapi server */
@@ -22,30 +23,23 @@ server.route([
     method: 'GET',
     path: '/twilioCall',
     handler: function(request, reply) {
-      var twilioRequest = request.query;
 
-      /* Parse the request body */
-      var twilioBody = request.query.Body.split(' ');
-      twilioBody = twilioBody.map(function(element) {
-        return element.toLowerCase().trim();
-      });
+      helper.parseRequestBody(request, function(err, parsedData) {
 
-      var returnData = data[twilioBody[0]][twilioBody[1]][twilioBody[2]];
-      if (returnData instanceof Array) {
-        returnData = returnData.join('\n');
-      };
-
-      /* Text the client */
-      client.messages.create({
-        to: twilioRequest.From,
-        from: twilioRequest.To,
-        body: returnData
-      }, function(error, message) {
-        if (error) {
-          console.log(error.message);
+        if (!err) {
+          /* Text the client */
+          client.messages.create({
+            to: request.query.From,
+            from: request.query.To,
+            body: parsedData
+          }, function(error, message) {
+            if (error) {
+              console.log(error.message);
+            }
+          });
         }
-      });
 
+      });
     }
   }
 ]);
