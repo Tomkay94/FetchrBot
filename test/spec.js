e4a03def0d4d6da9f66305fe580190e872553872
karysto <thanasi.karachotzitis@mail.utoronto.ca>
2015-01-26 00:41:10 -0500

    Add skeleton for test suite.

---
+++ test/spec.js
@@ -0,0 +1,40 @@
+// var main = require('../index.js');
+
+exports.testRouteAccess = {
+  testTwilioAccessesRoute : function(test) {
+    test.done();
+  },
+  testURLCannotAccessTwilioRoute : function(test) {
+    test.done();
+  }
+};
+
+exports.testDataRequest = {
+  testOneWordData : function(test) {
+    // example: 'array'
+    test.done();
+  },
+  testTwoWordData : function(test) {
+    // example: 'linked list'
+    test.done();
+  },
+  testPhraseData : function(test) {
+    // example: 'good movie review'
+    test.done();
+  }
+};
+
+exports.testDataParsing = {
+  testOneWordData : function(test) {
+    // example: 'array'
+    test.done();
+  },
+  testTwoWordData : function(test) {
+    // example: 'linked list'
+    test.done();
+  },
+  testPhraseData : function(test) {
+    // example: 'good movie review'
+    test.done();
+  }
+};
